57012dae056baa8529571415dae2983404e3b09a
MohamedAhmedSD <btoox8python@gmail.com>
2023-05-19 21:02:58 +0200

    Procfiler

--- test.php
+++ test.php
@@ -1,3 +1,9 @@
 <?php 
 echo "btooooo";
+// we face error 403
+// so we add => to composer.json => "post-install-cmd" list
+// then on terminal we write => echo web: run this thing >Procfile
+
+
 ?>
+
