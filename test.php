ae2b691359ca46652dc531e07e12cb33e82f7286
MohamedAhmedSD <btoox8python@gmail.com>
2023-05-19 21:07:28 +0200

    Procfiler edit into utf8 txt

--- test.php
+++ test.php
@@ -5,5 +5,10 @@ echo "btooooo";
 // then on terminal we write => echo web: run this thing >Procfile
 
 
+//It is happening due to a different encoding type in your Procfile.
+// Open your Procfile in a text editor, preferably, Notepad.
+// Save the file and in the encoding option, change the encoding to UTF-8 (default is UTF-16).
+// Replace your current Procfile with this file in the root folder of your project and remove the .txt extension.
+
 ?>
 
